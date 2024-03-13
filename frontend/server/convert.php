62a7e1e8c1a62739cfec4d937dbccfec2824bd9d
roshanlimbu <birenlimbu415@gmail.com>
2024-03-13 07:59:32 +0545

    added coditional handling and output

--- frontend/server/convert.php
+++ frontend/server/convert.php
@@ -4,5 +4,15 @@ if (isset($_POST['temperature']) && isset($_POST['input-unit']) && isset($_POST[
   $inputTemp = floatval($_POST['temperature']);
   $inputUnit = $_POST['input-unit'];
   $outputUnit = $_POST['output-unit'];
-
+        if ($inputUnit === "celsius" && $outputUnit === "fahrenheit") {
+    $convertedTemp = ($inputTemp * 9/5) + 32;
+  } else if ($inputUnit === "fahrenheit" && $outputUnit === "celsius") {
+    $convertedTemp = ($inputTemp - 32) * 5/9;
+  } else {
+    $convertedTemp = $inputTemp; 
+  } 
+  echo json_encode(array('convertedTemp' => $convertedTemp));
+  } else {
+  echo "Error: Invalid data received.";
+}
 ?>
